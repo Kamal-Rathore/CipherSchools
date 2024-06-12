d24742fdf857df67aa52539cd82bfd89ce726b16
Kamal Singh Rathore <127128416+Kamal-Rathore@users.noreply.github.com>
2024-06-13 00:26:51 +0530

    Lecture3-ProgrammingWithJava-CipherSchools

---
+++ PrimitiveDT.java
@@ -0,0 +1,18 @@
+public class PrimitiveDT{
+    public static void main(String[] args){
+        int x=5;
+        int y=7;
+        System.out.println("x is: "+x);
+        System.out.println("y is: "+y);
+        byte a=10;
+        System.out.println("a is: "+a);
+        byte a1=(byte)129;
+        System.out.println("a1 is: "+a1);
+        int i=a1;
+        System.out.println("I is: "+i);
+
+
+
+        
+    }
+}
