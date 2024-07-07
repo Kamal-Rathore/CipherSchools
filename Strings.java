d6d0dd7e4cc902cbf5d802bd0b94fc3e36fc8c02
Kamal Singh Rathore <127128416+Kamal-Rathore@users.noreply.github.com>
2024-07-07 13:28:17 +0530

    Lecture23-Strings-pt2-CipherSchools

--- Strings.java
+++ Strings.java
@@ -1,29 +1,87 @@
+import java.util.Scanner;
 public class Strings {
     public static void main(String [] args){
-        String name="CipherSchools*"; // created using string literals
-                String name2 = "CipherSchools";
-                String name3 = new String("CipherSchools");
-                String name4 = new String("CipherSchools");
-                System.out.println(name==name2);
-                System.out.println(name3==name4);
-                System.out.println(name==name3);
+        Scanner sc = new Scanner(System.in);
+        // String name="CipherSchools*"; // created using string literals
+        //         String name2 = "CipherSchools";
+        //         String name3 = new String("CipherSchools");
+        //         String name4 = new String("CipherSchools");
+        //         System.out.println(name==name2);
+        //         System.out.println(name3==name4);
+        //         System.out.println(name==name3);
         
         
-                String s1="hello";
-                s1= s1+"ppers";
-                System.out.println(s1);
+        //         String s1="hello";
+        //         s1= s1+"ppers";
+        //         System.out.println(s1);
         
-                String s2="hello";
-                String s3 = "concat";
-                String s4=s2.concat(s3);
-                System.out.println(s4);
+        //         String s2="hello";
+        //         String s3 = "concat";
+        //         String s4=s2.concat(s3);
+        //         System.out.println(s4);
         
-                System.out.println(s2.equals(s1));
+        //         System.out.println(s2.equals(s1));
         
         
-                char arr[]={'a','b','v','e'};
-                String s5 = new String(arr);
-                System.out.println(s5);
+        //         char arr[]={'a','b','v','e'};
+        //         String s5 = new String(arr);
+        //         System.out.println(s5);
+
+// char arr[]={'C','I','P','H','E','R'};
+// String partialstring  = new String(arr,1,3);
+// System.out.println(partialstring);
+
+// String name="cipher";
+// System.out.println(name.toUpperCase());
+// String name1="CIPHER";
+// System.out.println(name1.toLowerCase());
+
+// //splitting
+// System.out.println("enter your full name");
+// String fullname  = sc.nextLine();
+// String arr1[] = fullname.split(" ");
+// for(int i=0;i<arr1.length;i++){
+//     System.out.println(arr1[i]);
+    
+// }
+
+
+// // splitting in the comma separated file
+// System.out.println("enter your full name");
+// String fullname1  = sc.nextLine();
+// String arr2[] = fullname1.split(",");
+// for(int i=0;i<arr2.length;i++){
+//     System.out.println(arr2[i]);
+    
+// }
+
+
+// String fullname2  = "hii this is \"java\" class"; // escape sequnces
+// String arr3[] = fullname2.split("[.]");
+// for(int i=0;i<arr3.length;i++){
+//     System.out.println(arr3[i]); //java class 
+    
+// }
+// String str="this is cipher";
+
+// int  length1 = str.length();
+// System.out.println("length of the string "+str+"is:"+length1); 
+
+
+// String str="this is cipher";
+// int index = str.indexOf('e');
+// System.out.println("index of p is: "+index);
+
+// String str="this is cipher";
+// int index = str.indexOf("cipher");
+// System.out.println("index of p is: "+index);
+
+// while(indexOfo!=-1){
+//     indexOfo = name.indexOfo('o',indexOfo+1);
+
+//     if(indexOfo==-1)break;
+//     System.out.println(indexOfo);
+// }
 
     }
 }
