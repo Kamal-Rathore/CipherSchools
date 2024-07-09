4925cda7673e5875a0d7f4d304a7ff06a1782fd4
Kamal Singh Rathore <127128416+Kamal-Rathore@users.noreply.github.com>
2024-07-09 13:43:26 +0530

    Lecture27-TwoPointerConcept-CipherSchools

---
+++ Two_pointer_concept.java
@@ -0,0 +1,23 @@
+public class Two_pointer_concept {
+    public static void main(String[]  args){
+        int arr[]={10,20,35,50,70,80};
+    //     int x=95;
+    //     int l=0;
+    //     int r=arr.length-1;
+    //     while(l<r){
+    //         int sum=arr[l]+arr[r];
+    //         if(sum==x){
+    //             System.out.println("found "+x+"as a sum of values index "+l+"and index"+r);
+    //             return;
+    //         }
+    //         else(sum<x){
+    //             l++;
+    //         }
+    //         else{
+    //             r--;
+    //         }
+    //     }
+    // }
+    // System.out.println("sum not found");
+}
+}
